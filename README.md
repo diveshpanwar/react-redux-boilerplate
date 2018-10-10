c46888299aa1f17fd5db935226f8d8ed1d25039c
Divesh Panwar <divesh.panwar@gmail.com>
2018-10-11 00:22:41 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# React Redux Boilerplate
+
+Boilerplate and guide.
+
+## Getting Started
+
+To get started, first install all the necessary dependencies.
+```
+> npm install
+```
+
+Start the development server (changes will now update live in browser)
+```
+> npm run start
+```
+
+To view your project, go to: [http://localhost:7777/](http://localhost:7777/)
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devServer: {
+    inline: true,
+    contentBase: './src',
+    port: 7777
+  },
+  mode: 'development',
+  entry: './dev/js/main.js',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      },
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss/,
+        loader: 'style-loader!css-loader!sass-loader'
+      }
+    ]
+  },
+  output: {
+    path: 'src',
+    filename: 'js/bundle.min.js'
+  },
+  plugins: [new webpack.optimize.OccurrenceOrderPlugin()]
+};
